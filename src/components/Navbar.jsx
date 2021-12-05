4fb9f470c14da604e421a4d8215a347bd44948e6
Suvojit Ray <suvojit.royuvo@gmail.com>
2021-12-05 15:48:05 +0530

    navbar_reponsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,55 @@ import { isBrowser } from "react-device-detect";
 import "./style/Navbar.css";
 import ham from "./style/images/menu.png";
 import Drawerroutes from "./DrawerRoutes";
+import Logo from "./style/images/logo.svg";
+import styled from "styled-components";
+
+import { Button, Gradient } from "./style/Home";
+const Navdiv = styled.div`
+  @media (min-width: 1200px) {
+    display: none;
+  }
+`;
 const NavBar = (props) => {
-  const [showDrawer, setShowDrawer] = useState(false);
-//   const name = props.location.pathname.replaceAll("-", " ").replace("/", "");
   return (
-    
-      <div
-        className="header-container"
+    <Navdiv>
+      <nav
+        role="navigation"
+        style={{ display: "flex", justifyContent: "space-between" }}
       >
-        <div className="header-ham" style={{width:"100vw",display:"flex",justifyContent:"space-between",alignItems:"center"}} >
+        <div id="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <a href="#">
+              <li>Team and Advisors</li>
+            </a>
+            <a href="#">
+              <li>NFT</li>
+            </a>
+            <a href="#">
+              <li>Swap</li>
+            </a>
+            <a href="#">
+              <li>Contact</li>
+            </a>
+            <a href="https://erikterwan.com/" target="_blank">
+              <li>Login</li>
+            </a>
+            <Button style={{backgroundColor:"black",margin:"",padding:"0.5rem",width:"fit-content"}}>
+              Connect Wallet
+            </Button>
+          </ul>
+        </div>
+        <div>
+          <img src={Logo} alt="logo" />
+        </div>
+      </nav>
+      {/* <div className="header-ham" style={{width:"100vw",display:"flex",justifyContent:"space-between",alignItems:"center"}} >
           <div><img className="menubutton" alt="ham" src={ham} style={{opacity:0.4}} onClick={() => setShowDrawer(true)}></img></div>
           
         </div>
@@ -33,8 +73,8 @@ const NavBar = (props) => {
           {...props}
           closeDrawer={() => setShowDrawer(false)}
         ></Drawerroutes>
-      </Drawer>
-    </div>
+      </Drawer> */}
+    </Navdiv>
   );
 };
 
